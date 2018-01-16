b890c87ac0970209b9939846f7d17400a3a46d0c
matheuscmacedo0 <matheuscmacedo@hotmail.com>
2018-01-16 21:46:31 -0200

    Comunicando com servidor v1.1

--- noticias.js
+++ noticias.js
@@ -1,7 +1,20 @@
 var http = require('http');
 
+
+
 var server = http.createServer( function(req,res){
-	res.end('<html><body>Portal de Notícias</body></html>');
+	var categoria = req.url;
+
+	if(categoria == '/tecnologia'){
+		res.end('<html><body>Portal de Tecnologia</body></html>');
+	}else if(categoria == '/moda'){
+		res.end('<html><body>Portal de Moda</body></html>');
+	}else if(categoria == '/beleza'){
+		res.end('<html><body>Portal de beleza</body></html>');
+	}else{
+		res.end('<html><body>Portal de Noticias</body></html>');
+	}
+	
 
 });
 
