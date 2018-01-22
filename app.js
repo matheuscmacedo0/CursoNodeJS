076b9f98b41b32f15e7a2a09b927326c5c60e0d0
matheuscmacedo0 <matheuscmacedo@hotmail.com>
2018-01-22 21:24:25 -0200

    Refactoring com EJS v1.0

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('/tecnologia', function(req, res) {
-	res.render("secao/tecnologia");
+app.get('/', function(req, res) {
+	res.render("home/index");
 });
 
-app.get('/moda', function(req, res) {
-	res.render("secao/moda");
+app.get('/formulario_inclusao_noticia', function(req, res) {
+	res.render("admin/form_add_noticia");
 });
 
-app.get('/beleza', function(req, res) {
-	res.render("secao/beleza");
+app.get('/noticias', function(req, res) {
+	res.render("noticias/noticias");
 });
 
 app.get('/', function(req, res) {
